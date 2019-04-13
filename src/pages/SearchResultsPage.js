bf068df2d10eae89d02a876f31499a476192a586
Jackie Xu <jackiexubm1@gmail.com>
2019-04-13 11:30:39 -0700

    Edit mock data to include upvotes, calls, and pricing

--- src/pages/SearchResultsPage.js
+++ src/pages/SearchResultsPage.js
@@ -9,31 +9,39 @@ class SearchResultsPage extends Component {
     		description:
     			'A CNN based pytorch implementation on facial expression recognition (FER2013 and CK+), achieving 73.112% (state-of-the-art) in FER2013 and 94.64% in CK+ dataset',
     		updated: '7 months ago',
+        pricing: '$0.10/1000 calls',
     		tags: ['facial expression', 'emotion', 'face detection'],
-    		calls: '3.1M'
+    		calls: '5123',
+        upvotes: '1341'
     	},
     	{
     		title: 'Facial-Expression-Recognition',
     		description:
     			'Facial-Expression-Recognition in TensorFlow. Detecting faces in video and recognize the expression(emotion).',
     		updated: '9 months ago',
+        pricing: '$0.30/1000 calls',
     		tags: ['facial expression', 'emotion', 'video', 'face detection'],
-    		calls: '1.1M'
+        calls: '4211',
+        upvotes: '941'
     	},
     	{
     		title: 'Expression-Net',
     		description:
     			'Deep 3DMM facial expression parameter extraction. This contains a deep convolutional neural network (DCNN) model for robust estimation of the 29 degrees-of-freedom, 3DMM face expression coefficients, directly from an unconstrained face image and without the use of face landmark detectors.',
     		updated: '6 months ago',
+        pricing: '$2/1000 calls',
     		tags: ['facial expression', 'emotion', 'face detection'],
-    		calls: '2.8M'
+        calls: '51041',
+        upvotes: '2341'
     	},
     	{
     		title: 'emojify',
     		description: 'Turn your facial expression into an emoji',
     		updated: '11 months ago',
+        pricing: '$0.6/1000 calls',
     		tags: ['facial expression', 'emotion', 'face detection', 'emoji'],
-    		calls: '800k'
+        calls: '5123',
+        upvotes: '1341'
     	}
     ];
 
