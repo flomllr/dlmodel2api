632d47f650330d744e011716fb649d146004ca86
Jackie Xu <jackiexubm1@gmail.com>
2019-04-13 11:17:31 -0700

    Separate SearchResults into page and component parts

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SearchResults from './SearchResult';
+import SearchResults from '../components/SearchResults';
 import { Button, Card, Elevation } from '@blueprintjs/core';
 import './App.css';
 
---
+++ src/pages/SearchResultsPage.js
@@ -0,0 +1,51 @@
+import SearchResults from '../components/SearchResults';
+import React, { Component } from 'react';
+
+class SearchResultsPage extends Component {
+	render() {
+    const searchMockData = [
+    	{
+    		title: 'Facial-Expression-Recognition.Pytorch',
+    		description:
+    			'A CNN based pytorch implementation on facial expression recognition (FER2013 and CK+), achieving 73.112% (state-of-the-art) in FER2013 and 94.64% in CK+ dataset',
+    		updated: '7 months ago',
+    		tags: ['facial expression', 'emotion', 'face detection'],
+    		calls: '3.1M'
+    	},
+    	{
+    		title: 'Facial-Expression-Recognition',
+    		description:
+    			'Facial-Expression-Recognition in TensorFlow. Detecting faces in video and recognize the expression(emotion).',
+    		updated: '9 months ago',
+    		tags: ['facial expression', 'emotion', 'video', 'face detection'],
+    		calls: '1.1M'
+    	},
+    	{
+    		title: 'Expression-Net',
+    		description:
+    			'Deep 3DMM facial expression parameter extraction. This contains a deep convolutional neural network (DCNN) model for robust estimation of the 29 degrees-of-freedom, 3DMM face expression coefficients, directly from an unconstrained face image and without the use of face landmark detectors.',
+    		updated: '6 months ago',
+    		tags: ['facial expression', 'emotion', 'face detection'],
+    		calls: '2.8M'
+    	},
+    	{
+    		title: 'emojify',
+    		description: 'Turn your facial expression into an emoji',
+    		updated: '11 months ago',
+    		tags: ['facial expression', 'emotion', 'face detection', 'emoji'],
+    		calls: '800k'
+    	}
+    ];
+
+    console.log(searchMockData);
+    return (
+      <div>
+        <br />
+        <h2>Showing models related to facial recognition</h2>
+        <SearchResults mockData={searchMockData} />
+      </div>
+    );
+  }
+}
+
+export default SearchResultsPage;
