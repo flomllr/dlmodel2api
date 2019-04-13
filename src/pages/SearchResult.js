de541c02312b2b79145ff94ebd6143c11a1c2ae2
Jackie Xu <jackiexubm1@gmail.com>
2019-04-13 05:47:42 -0700

    Improve SearchResult card to reflect JSON data

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -48,26 +48,32 @@ class SearchResultCard extends Component {
 
 	render() {
 		const { resultData } = this.props;
+		const cardTitle = resultData["title"];
+		const cardDesc = resultData["description"];
+		const cardTags = resultData["tags"];
+		const cardCallsCount = resultData["calls"];
+
+		console.log(cardTags);
 
 		return (
 			<Card className='infoCard' {...this.cardOptions} style={cardStyles}>
 				<div style={cardCol1}>
 					<H4>
 						<a className='cardTitle' href='/overview'>
-							Analytical applications
+							{cardTitle}
 						</a>
 					</H4>
 					<p>
-						User interfaces that enable people to interact smoothly with data,
-						ask better questions, and make better decisions. Long description
-						ong description user interfaces that enable people to interact
-						smoothly with data, ask better questions, and make better decisions.
+						{cardDesc}
 					</p>
 					{/*<p style={finePrintStyle}>More sample sample sample sample text</p>*/}
 					<div className='tagList'>
-						<div className='tag'>Speech-to-text</div>
-						<div className='tag'>Natural language</div>
-						<div className='tag'>API</div>
+						<React.Fragment>
+							{cardTags.map(tag => (
+								<div className='tag'>{tag}</div>
+							))}
+						</React.Fragment>
+
 					</div>
 				</div>
 
@@ -83,13 +89,44 @@ class SearchResultCard extends Component {
 
 class SearchResult extends Component {
 	render() {
-		const mockData = ['a', 'b', 'c'];
+		const mockData = [
+			{
+					"title":"DeepSpeech",
+					"description":"DeepSpeech is an open source Speech-To-Text engine, using a model trained by machine learning techniques based on Baidu's Deep Speech research paper. Project DeepSpeech uses Google's TensorFlow to make the implementation easier.",
+					"updated":"a day ago",
+					"tags":["audio", "speech", "text", "NLP"],
+					"calls":"2.8M"
+			},
+			{
+					"title":"video-classification-3d-cnn-pytorch",
+					"description":"This is a pytorch code for video (action) classification using 3D ResNet trained by this code. The 3D ResNet is trained on the Kinetics dataset, which includes 400 action classes. This code uses videos as inputs and outputs class names and predicted class scores for each 16 frames in the score mode. In the feature mode, this code outputs features of 512 dims (after global average pooling) for each 16 frames.",
+					"updated":"6 months ago",
+					"tags":["video", "classification", "action"],
+					"calls":"1.9M"
+			},
+	    {
+	        "title":"ssd.pytorch",
+	        "description":"A PyTorch implementation of Single Shot MultiBox Detector from the 2016 paper by Wei Liu, Dragomir Anguelov, Dumitru Erhan, Christian Szegedy, Scott Reed, Cheng-Yang, and Alexander C. Berg.",
+	        "updated":"a month ago",
+	        "tags":["classification", "images", "single-shot", "multibox"],
+	        "calls":"2.1M"
+	    },
+	    {
+	        "title":"faster-rcnn.pytorch",
+	        "description":"This project is a faster pytorch implementation of faster R-CNN, aimed to accelerating the training of faster R-CNN object detection models.",
+	        "updated":"18 days ago",
+	        "tags":["classification", "images"],
+	        "calls":"3.1M"
+	    }
+
+		];
+
 		const resultsData = [];
 
 		return (
 			<React.Fragment>
 				{mockData.map(e => (
-					<SearchResultCard />
+					<SearchResultCard resultData={e}/>
 				))}
 			</React.Fragment>
 		);
