386803a35eada2fd73af2eb5f13e6065a0906b9f
Florian Mueller <contact@flomllr.com>
2019-04-13 11:59:58 -0700

    Change title

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -53,7 +53,7 @@ class PreTest extends Component {
 class Overview extends Component {
 	state = {
 		modelName: 'Facial-Expression-Recognition',
-		url: 'https://api.deephub.ai/Facial-Expression-Recognition/v1',
+		url: 'https://api.deeplify.ai/Facial-Expression-Recognition/v1',
 		abstract: `A CNN based pytorch implementation on facial expression recognition (FER2013 and CK+), achieving 73.112% (state-of-the-art) in FER2013 and 94.64% in CK+ dataset`,
 		inputs: [{ name: 'image', type: 'image' }],
 		outputs: [{ name: 'labels', type: 'string' }],
